48b53bcb5f29a97403f39087cb6667b58111730f
markwasilwa <KEN21908@kcbgroup.com>
2023-01-10 20:17:27 +0300

    initial checkin

---
+++ src/main/java/markwasilwa/wmmx/JavaXmlDomReadEx.java
@@ -0,0 +1,51 @@
+package markwasilwa.wmmx;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
+public class JavaXmlDomReadEx {
+
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+        File xmlFile = new File("src/main/resources/users.xml");
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(xmlFile);
+
+        doc.getDocumentElement().normalize();
+        System.out.println("Root element : " + doc.getDocumentElement().getNodeName());
+
+        NodeList nodeList = doc.getElementsByTagName("user");
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node nNode = nodeList.item(i);
+            System.out.println("\nCurrent Element: " + nNode.getNodeName());
+
+            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element elem = (Element) nNode;
+                String uid = elem.getAttribute("id");
+                Node node1 = elem.getElementsByTagName("firstname").item(0);
+                String fname = node1.getTextContent();
+
+                Node node2 = elem.getElementsByTagName("lastname").item(0);
+                String lname = node2.getTextContent();
+
+                Node node3 = elem.getElementsByTagName("occupation").item(0);
+                String occup = node3.getTextContent();
+
+                System.out.printf("User id: %s%n", uid);
+                System.out.printf("First name: %s%n", fname);
+                System.out.printf("Last name: %s%n", lname);
+                System.out.printf("Occupation: %s%n", occup);
+            }
+        }
+    }
+}
---
+++ src/main/java/markwasilwa/wmmx/JavaXmlDomReadText.java
@@ -0,0 +1,36 @@
+package markwasilwa.wmmx;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.traversal.DocumentTraversal;
+import org.w3c.dom.traversal.NodeFilter;
+import org.w3c.dom.traversal.NodeIterator;
+import org.xml.sax.SAXException;
+
+import javax.print.Doc;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
+public class JavaXmlDomReadText {
+
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse("src/main/resources/continents.xml");
+
+        DocumentTraversal traversal = (DocumentTraversal) document;
+
+        NodeIterator iterator = traversal.createNodeIterator(
+                document.getDocumentElement(), NodeFilter.SHOW_ELEMENT, null, true);
+
+        for (Node n = iterator.nextNode(); n != null; n = iterator.nextNode()) {
+            String text = n.getTextContent().trim();
+
+            if (!text.isEmpty()) {
+                System.out.println(text);
+            }
+        }
+    }
+}
